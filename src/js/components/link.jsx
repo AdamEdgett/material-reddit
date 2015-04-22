9cd3eb3b5e7c43f115913aabb1dcdb3285231fb7
Adam Edgett <AdamEdgett@gmail.com>
2015-04-22 10:38:56 -0400

    Add thumbnail to links

--- src/js/components/link.jsx
+++ src/js/components/link.jsx
@@ -17,27 +17,34 @@ const Link = React.createClass({
     url: React.PropTypes.string,
     name: React.PropTypes.string,
     createdUtc: React.PropTypes.number,
-    permalink: React.PropTypes.string
+    permalink: React.PropTypes.string,
+    thumbnail: React.PropTypes.string
   },
 
   render: function() {
-    const { url, title, createdUtc, author, subreddit, permalink, numComments } = this.props;
+    const { url, title, createdUtc, author, subreddit, permalink, thumbnail, numComments } = this.props;
 
     const created = moment.unix(createdUtc);
+    const thumbnailSrc = _.isEqual(thumbnail, 'self') ?  '' : thumbnail;
     return (
       <div className='link-container card'>
-        <a href={url} className='link'>{title}</a>
-        <div className='info'>
-          <div className='description'>
-            <span className='timestamp' title={created.toString()}>{`Submitted ${created.fromNow()}`}</span>
-            <span className='author'> by <a href={`/u/${author}`}>{author}</a></span>
-            <span className='subreddit'> on <a href={`/r/${subreddit}`}>{subreddit}</a></span>
-          </div>
-          <div className='controls'>
-            <a href={permalink} className='comments'>
-              <span className='count'>{numComments}</span>
-              <Icon type='comment' />
-            </a>
+        <div className='thumbnail'>
+          <img src={thumbnailSrc} />
+        </div>
+        <div className='content'>
+          <a href={url} className='link'>{title}</a>
+          <div className='info'>
+            <div className='description'>
+              <span className='timestamp' title={created.toString()}>{`Submitted ${created.fromNow()}`}</span>
+              <span className='author'> by <a href={`/u/${author}`}>{author}</a></span>
+              <span className='subreddit'> on <a href={`/r/${subreddit}`}>{subreddit}</a></span>
+            </div>
+            <div className='controls'>
+              <a href={permalink} className='comments'>
+                <span className='count'>{numComments}</span>
+                <Icon type='comment' />
+              </a>
+            </div>
           </div>
         </div>
       </div>
