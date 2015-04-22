852c5b9f4fe0954001b51e49d82e5cf49eebdf81
Adam Edgett <AdamEdgett@gmail.com>
2015-04-22 17:07:28 -0400

    Add optional size and className props to icon

--- src/js/components/icon.jsx
+++ src/js/components/icon.jsx
@@ -1,13 +1,24 @@
 const React = require('react');
+const _ = require('lodash');
+const classnames = require('classnames');
 
 const Icon = React.createClass({
   propTypes: {
-    type: React.PropTypes.string.isRequired
+    type: React.PropTypes.string.isRequired,
+    size: React.PropTypes.string,
+    className: React.PropTypes.string
   },
 
   render: function() {
-    const { type } = this.props;
-    return <i className={`fa fa-${type}`} />;
+    const { type, size, className } = this.props;
+
+    let classes = {};
+    classes[className] = !_.isEmpty(className);
+    classes[type] = true;
+    classes[`fa fa-${type}`] = true;
+    classes[`fa-${size}`] = !_.isEmpty(size);
+
+    return <i className={classnames(classes)} />;
   }
 });
 
