fe4e503523fb297bb3dcad377e45ed50f36340e0
Adam Edgett <AdamEdgett@gmail.com>
2015-04-23 01:14:05 -0400

    Fix route for subreddit

--- src/js/routes.js
+++ src/js/routes.js
@@ -5,19 +5,17 @@ const Routes = {
     target: SubredditTarget,
     '/': 'view',
   },
+  '/r/:subreddit': {
+    target: SubredditTarget,
+    '/': 'view',
+    '/:sort': {
+      '/': 'view',
+      '/:time': 'view'
+    }
+  },
   '/:sort': {
     target: SubredditTarget,
     '/': 'view'
-  },
-  '/r': {
-    target: SubredditTarget,
-    '/:subreddit': {
-      '/': 'view',
-      '/:sort': {
-        '/': 'view',
-        '/:time': 'view'
-      }
-    }
   }
 };
 
